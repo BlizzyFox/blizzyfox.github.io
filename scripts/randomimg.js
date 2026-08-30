d54c6a88732aefdbd739e3cbefc796de4f2f6c36
Blizzy Fox (windows) <mitchellwezley@gmail.com>
2026-08-30 14:08:57 -0400

    changed images

--- scripts/randomimg.js
+++ scripts/randomimg.js
@@ -1,7 +1,7 @@
 const images = [
     'https://cdnb.artstation.com/p/assets/images/images/099/262/195/large/wezley-mitchell-vrchat-2026-05-15-04-54-11-587-3840x2160-large.jpg?1779403049',
-    'https://d.furaffinity.net/art/blizzyfoxo/1784861561/1784861561.blizzyfoxo_poolparty__medium_.png',
-    'https://d.furaffinity.net/art/blizzyfoxo/1788112186/1788112186.blizzyfoxo_newfacerenderw.jpg'
+    'https://d.furaffinity.net/art/blizzyfoxo/1788112186/1788112186.blizzyfoxo_newfacerenderw.jpg',
+    'https://d.furaffinity.net/art/blizzyfoxo/1787121903/1787121825.blizzyfoxo_fireworkswatermark.jpg'
 ];
 
 document.addEventListener('DOMContentLoaded', () => {
